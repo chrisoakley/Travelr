8091e3c01882e6dd6998254e308ebbdd9e1c0f5e
Chris <hello@chrisoakley.me>
2012-12-09 10:37:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelr::Application.config.secret_token = 'da7b939338b7fb9e4de92e407e8e27ac7e76fbe62f391e4e7c8a3256e5ff809fd689c2a0dd811cb3dd685208f808afe9bced234f725fcc5de7d5fb995c924477'
